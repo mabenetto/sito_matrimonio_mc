f2e8053e86ddac5b0ed6b3e73bfaa10ff66e5451
root <root@ip-172-31-24-142.eu-west-1.compute.internal>
2017-12-29 10:20:53 +0000

    aaa

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -13,8 +13,9 @@ $email_address = strip_tags(htmlspecialchars($_POST['email']));
 $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
 
+echo $name;
 // Create the email and send the message
-$to = 'mabenett@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$to = "mabenett@gmail.com"; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Conferma Presenza Matrimonio:  $name";
 $email_body = "$name\n ha confermato la presenza al matrimonio: $email_address\n\nPhone: $phone\n\n Messaggio:\n$message";
 $headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
