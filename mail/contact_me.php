8e00ce607b81781eb8e28960a739d97727704539
root <root@ip-172-31-24-142.eu-west-1.compute.internal>
2017-12-31 15:41:56 +0000

    ritorno a mail

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,8 +1,4 @@
 <?php
-require 'classi/PHPMailer.php';
-require 'classi/Exception.php';
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
 // Check for empty fields
 if(empty($_POST['name'])      ||
    empty($_POST['email'])     ||
@@ -11,30 +7,12 @@ if(empty($_POST['name'])      ||
    echo "No arguments Provided!";
    return false;
    }
-
-$name = strip_tags(htmlspecialchars($_POST['name']));
-$email_address = strip_tags(htmlspecialchars($_POST['email']));
-$phone = strip_tags(htmlspecialchars($_POST['phone']));
-$message = strip_tags(htmlspecialchars($_POST['message']));
-
-$email_subject = "Conferma Presenza Matrimonio:  $name";
-$email_body = "$name\n ha confermato la presenza al matrimonio: $email_address\n\nPhone: $phone\n\n Messaggio:\n$message";
-
-$mail = new PHPMailer;
-
-$mail->setFrom($email_address, $name);
-$mail->addAddress('mabenett@gmail.com', 'Marco')
-$mail->addAddress('chia.soriani@gmail.com', 'Chiara')
-
-$mail->Subject = $email_subject;    
-$mail->Body = $email_body;
-
-if (!$mail->send()) {
-    //The reason for failing to send will be in $mail->ErrorInfo
-    //but you shouldn't display errors to users - process the error, log it on your server.
-    $msg = 'Sorry, something went wrong. Please try again later.';
-} else {
-    $msg = 'Message sent! Thanks for contacting us.';
-}
-echo $msg;         
+$name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+$from = $_POST['email']; 
+$to = 'mabenett@gmail.com '; 
+$subject = 'conferma invitato: $name';
+$body = "From:".$name."\n E-Mail: ".$email."\n Message:\n ".$message;
+mail($to,$subject,$body);
 ?>
