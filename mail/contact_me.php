54c67c0894321cb0293bbccc9b013061efa1abb2
DESKTOP-RCS16HO\maben <mabenett@gmail.com>
2017-12-31 17:00:54 +0100

    ritentativo phpmailer

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -13,6 +13,36 @@ $message = $_POST['message'];
 $from = $_POST['email']; 
 $to = 'info@marcoechiara2018.it'; 
 $subject = 'conferma invitato:'.$name;
-$body = "From:".$name."\n E-Mail: ".$email."\n Message:\n ".$message;
-mail($to,$subject,$body);
+
+//mail($to,$subject,$body);
+
+
+include("classi/PHPMailer.php"); // inclusione della classe PHPMailer
+$mittente = "info@marcoechiara2018.it";
+$nomemittente = "Marco e Chiara";
+$to = 'info@marcoechiara2018.it'; 
+$ServerSMTP = "smtps.aruba.it"; //server SMTP
+$corpo_messaggio = "From:".$name."\n E-Mail: ".$email."\n Message:\n ".$message;
+
+$msg = new PHPMailer;
+$msg->IsSMTP(); // Utilizzo della classe SMTP al posto del comando php mail()
+$msg->SMTPAuth = true; // Autenticazione SMTP
+$msg->SMTPKeepAlive = "true";
+$msg->Host = $ServerSMTP;
+$msg->Username = "info@marcoechiara2018.it"; // Nome utente SMTP autenticato
+$msg->Password = "pasquetta"; // Password account email con SMTP autenticato
+
+$msg->Subject  = 'conferma invitato:'.$name;
+$msg->From = $mittente;
+$msg->FromName = $nomemittente;
+$msg->AddAddress($destinatario); 
+$msg->Body = $corpo_messaggio;
+if(!$msg->Send()) {
+echo "errore nella spedizione: ".$msg->ErrorInfo;
+return false;
+} else {
+echo "Il messaggio di posta è stato inviato correttamente";
+return true;
+}
+
 ?>
