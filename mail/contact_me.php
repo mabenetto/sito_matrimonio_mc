36f165a97c350a64c640cdf46d0d517bd3c120d5
DESKTOP-RCS16HO\maben <mabenett@gmail.com>
2017-12-31 17:30:12 +0100

    classe mail

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,48 +1,40 @@
 <?php
-// Check for empty fields
-if(empty($_POST['name'])      ||
-   empty($_POST['email'])     ||
-   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
-   {
-   echo "No arguments Provided!";
-   return false;
-   }
-$name = $_POST['name'];
-$email = $_POST['email'];
+header("access-control-allow-origin: *");
+
+require_once("classi/classe_mail.php");
+
+
+
+
+
+$dati_mittente = json_decode('{
+    "mail_mitt": "info@marcoechiara2018.it",
+    "nome_mitt": "App",
+    "to": "info@marcoechiara2018.it",
+    "server_smtp": "smtps.aruba.it",
+    "username": "info@marcoechiara2018.it",
+    "password": "pasquetta",
+    "use_postman": false,
+    "postman_client_id": "user_name",
+    "postman_client_secret": "user_Secret"
+    }');
+
+$mail  = new classe_mail(0,false);
+
+$nome = $_POST['name'];
+$mail = $_POST['email'];
 $message = $_POST['message'];
-$from = $_POST['email']; 
-$to = 'info@marcoechiara2018.it'; 
-$subject = 'conferma invitato:'.$name;
-
-//mail($to,$subject,$body);
+$token = $_POST['token'];
 
 
-include("classi/PHPMailer.php"); // inclusione della classe PHPMailer
-$mittente = "info@marcoechiara2018.it";
-$nomemittente = "Marco e Chiara";
-$to = 'info@marcoechiara2018.it'; 
-$ServerSMTP = "smtps.aruba.it"; //server SMTP
-$corpo_messaggio = "From:".$name."\n E-Mail: ".$email."\n Message:\n ".$message;
+$mail->set_param($dati_mittente->nome_mitt, $dati_mittente->mail_mitt, $dati_mittente->server_smtp,465, true, $dati_mittente->username, $dati_mittente->password);
 
-$msg = new PHPMailer;
-$msg->IsSMTP(); // Utilizzo della classe SMTP al posto del comando php mail()
-$msg->SMTPAuth = true; // Autenticazione SMTP
-$msg->SMTPKeepAlive = "true";
-$msg->Host = $ServerSMTP;
-$msg->Username = "info@marcoechiara2018.it"; // Nome utente SMTP autenticato
-$msg->Password = "pasquetta"; // Password account email con SMTP autenticato
+$mail->set_text("Conferma presenza da: ".$nome, "From:".$name."\n E-Mail: ".$email."\n Message:\n ".$message);
+$mail->add_dest($dati_mittente->to);
 
-$msg->Subject  = 'conferma invitato:'.$name;
-$msg->From = $mittente;
-$msg->FromName = $nomemittente;
-$msg->AddAddress($destinatario); 
-$msg->Body = $corpo_messaggio;
-if(!$msg->Send()) {
-echo "errore nella spedizione: ".$msg->ErrorInfo;
-return false;
-} else {
-echo "Il messaggio di posta è stato inviato correttamente";
-return true;
+
+if( $token == "mytokenmarcoechiara"){
+	$esito = $mail->send();
 }
 
-?>
+echo json_encode(array("status"=>$esito));
