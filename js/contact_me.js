4b2c6a551dbf38c1c04a3be52fcc0294d97f2fac
DESKTOP-RCS16HO\maben <mabenett@gmail.com>
2018-01-02 12:35:12 +0100

    prima versione rsvp rotto

--- js/contact_me.js
+++ js/contact_me.js
@@ -47,7 +47,7 @@ $(function() {
           $('#success').html("<div class='alert alert-danger'>");
           $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
             .append("</button>");
-          $('#success > .alert-danger').append($("<strong>").text("Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!"));
+          $('#success > .alert-danger').append($("<strong>").text("Ehi " + firstName + ", c'è un problema con la conferma della mail! Per favore conferma mandando una mail a info@marcoechiara2018.it ! Grazie Mille!!!"));
           $('#success > .alert-danger').append('</div>');
           //clear all fields
           $('#contactForm').trigger("reset");
