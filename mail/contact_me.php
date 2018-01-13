2ea71cbd13bcff2611e572fc66ffc51d58f00d0a
DESKTOP-RCS16HO\maben <mabenett@gmail.com>
2018-01-13 11:41:00 +0100

    impostazione mail aws

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,37 +1,79 @@
 <?php
-header("access-control-allow-origin: *");
-
-
-
-
-
-
-/*$dati_mittente = json_decode('{
-    "mail_mitt": "info@marcoechiara2018.it",
-    "nome_mitt": "App",
-    "to": "info@marcoechiara2018.it",
-    "server_smtp": "smtp.marcoechiara2018.it",
-    "username": "info@marcoechiara2018.it",
-    "password": "pasquetta",
-    "use_postman": false,
-    "postman_client_id": "user_name",
-    "postman_client_secret": "user_Secret"
-    }');
-*/
-
 
 $nome = $_POST['name'];
 $email = $_POST['email'];
 $message = $_POST['message'];
 $token = $_POST['token'];
+$text = "Conferma presenza da: ".$nome."From:".$name."\n E-Mail: ".$email."\n Message:\n ".$message;
 
 
+// Replace path_to_sdk_inclusion with the path to the SDK as described in 
+// http://docs.aws.amazon.com/aws-sdk-php/v3/guide/getting-started/basic-usage.html
+define('REQUIRED_FILE','mail/mail_aws/aws-autoloader.php'); 
+                                                  
+// Replace sender@example.com with your "From" address. 
+// This address must be verified with Amazon SES.
+define('SENDER', 'info@marcoechiara2018.it');           
 
-$to ="info@marcoechiara2018.it";
-$subject = "Conferma presenza da: ".$nome;
-$message = "From:".$nome."\n E-Mail: ".$email."\n Message:\n ".$message;
-if(mail ($to, $subject, $message)){
-	echo "OK";
-}else{
-	echo "KO";
+// Replace recipient@example.com with a "To" address. If your account 
+// is still in the sandbox, this address must be verified.
+define('RECIPIENT', 'info@marcoechiara2018.it');    
+
+// Specify a configuration set. If you do not want to use a configuration
+// set, comment the following variable, and the 
+// 'ConfigurationSetName' => CONFIGSET argument below.
+
+
+// Replace us-west-2 with the AWS Region you're using for Amazon SES.
+define('REGION','us-west-2'); 
+
+define('SUBJECT','Conferma presenza da:'.$name);
+
+define('HTMLBODY',$text);
+define('TEXTBODY',$text);
+
+define('CHARSET','UTF-8');
+
+require REQUIRED_FILE;
+
+use Aws\Ses\SesClient;
+use Aws\Ses\Exception\SesException;
+
+$client = SesClient::factory(array(
+    'version'=> 'latest',     
+    'region' => REGION
+));
+
+try {
+     $result = $client->sendEmail([
+    'Destination' => [
+        'ToAddresses' => [
+            RECIPIENT,
+        ],
+    ],
+    'Message' => [
+        'Body' => [
+            'Html' => [
+                'Charset' => CHARSET,
+                'Data' => HTMLBODY,
+            ],
+			'Text' => [
+                'Charset' => CHARSET,
+                'Data' => TEXTBODY,
+            ],
+        ],
+        'Subject' => [
+            'Charset' => CHARSET,
+            'Data' => SUBJECT,
+        ],
+    ],
+    'Source' => SENDER,
+]);
+     $messageId = $result->get('MessageId');
+     echo("Email sent! Message ID: $messageId"."\n");
+
+} catch (SesException $error) {
+     echo("The email was not sent. Error message: ".$error->getAwsErrorMessage()."\n");
 }
+
+?>
