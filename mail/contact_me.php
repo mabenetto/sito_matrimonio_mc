5f0ed98f3ce266cbd850c2c6a4fe84e72a6cbc3e
root <root@ip-172-31-24-142.eu-west-1.compute.internal>
2018-01-16 08:35:40 +0000

    mail funzionante

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,15 +1,15 @@
 <?php
 
-$nome = $_POST['name'];
+$name = $_POST['name'];
 $email = $_POST['email'];
 $message = $_POST['message'];
 $token = $_POST['token'];
-$text = "Conferma presenza da: ".$nome."From:".$name."\n E-Mail: ".$email."\n Message:\n ".$message;
+$text = "Conferma presenza da: ".$name."From:".$name."\n E-Mail: ".$email."\n Message:\n ".$message;
 
 
 // Replace path_to_sdk_inclusion with the path to the SDK as described in 
 // http://docs.aws.amazon.com/aws-sdk-php/v3/guide/getting-started/basic-usage.html
-define('REQUIRED_FILE','mail/mail_aws/aws-autoloader.php'); 
+define('REQUIRED_FILE','mail_aws/aws-autoloader.php'); 
                                                   
 // Replace sender@example.com with your "From" address. 
 // This address must be verified with Amazon SES.
@@ -25,7 +25,7 @@ define('RECIPIENT', 'info@marcoechiara2018.it');
 
 
 // Replace us-west-2 with the AWS Region you're using for Amazon SES.
-define('REGION','us-west-2'); 
+define('REGION','eu-west-1'); 
 
 define('SUBJECT','Conferma presenza da:'.$name);
 
@@ -40,6 +40,7 @@ use Aws\Ses\SesClient;
 use Aws\Ses\Exception\SesException;
 
 $client = SesClient::factory(array(
+    'profile'=>'default',
     'version'=> 'latest',     
     'region' => REGION
 ));
